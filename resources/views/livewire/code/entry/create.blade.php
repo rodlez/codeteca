6272037f742b19547647b9ae2137ba042fb968cd
Zug <zugzwangez@gmail.com>
2024-10-23 17:10:44 +0200

    Styles Entry Create UI

--- resources/views/livewire/code/entry/create.blade.php
+++ resources/views/livewire/code/entry/create.blade.php
@@ -7,205 +7,175 @@
         </div>
         <div class="px-4">
             <button wire:click.prevent="help">
-                <i class="fa-lg fa-solid fa-circle-question text-white hover:text-black transition duration-1000 ease-in-out" title="help"></i>
+                <i class="fa-lg fa-solid fa-circle-question text-white hover:text-black transition duration-1000 ease-in-out"
+                    title="help"></i>
             </button>
         </div>
     </div>
 
-    <!-- Entry -->
+    <!-- New Entry -->
     <form wire:submit="save">
         <!-- Add Token to prevent Cross-Site Request Forgery (CSRF) -->
         @csrf
-        <!-- Title -->
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <h2 class="text-lg font-semibold py-2">Title <span class="text-red-400">*</span></h2>
+
+        <div class="mx-auto w-11/12">
+            <!-- Title -->
+            <h2 class="text-lg font-bold pt-4 pb-1 px-14">Title <span class="text-red-400">*</span></h2>
+            <div class="flex gap-2">
+                <i class="bg-zinc-200 p-3 rounded-lg fa-solid fa-pen-to-square"></i>
+                <input wire:model="title" name="title" id="title" type="text" value="{{ old('title') }}"
+                    class="w-full rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-green-500 focus:border-green-500">
             </div>
-            <div class="flex flex-row justify-start items-center gap-4">
-                <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                    <i class="fa-solid fa-pen-to-square"></i>
-                </span>
-                <input wire:model="title" name="title" id="title" type="text" value="{{ old('title') }}" class="w-full pl-4 p-2 text-md rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-orange-500 focus:border-orange-500">
+            <div class="text-sm text-red-600 font-bold px-14">
+                @error('title')
+                    {{ $message }}
+                @enderror
             </div>
-        </div>
-        <div class="py-2 px-20 sm:mx-12 text-red-600 font-semibold">
-            @error('title')
-                {{ $message }}
-            @enderror
-        </div>
-        <!-- Type -->
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <h2 class="text-lg font-semibold py-2">Type <span class="text-red-400">*</span></h2>
-            </div>
-            <div class="flex flex-row justify-start items-center gap-4">
-                <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                    <i class="fa-solid fa-sitemap"></i>
-                </span>
-                <select wire:model="type_id" name="type_id" id="type_id" class="w-full pl-4 p-2 text-md rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-orange-500 focus:border-orange-500">
+            <!-- Type -->
+            <h2 class="text-lg font-bold pt-4 pb-1 px-14">Type <span class="text-red-400">*</span></h2>
+            <div class="flex gap-2">
+                <i class="bg-zinc-200 p-3 rounded-lg fa-solid fa-sitemap"></i>
+                <select wire:model="type_id" name="type_id" id="type_id"
+                    class="w-full rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-green-500 focus:border-green-500">
                     @foreach ($types as $type)
-                        <option value="{{ $type->id }}" class="text-orange-500"
-                                @if (old('type_id') == $type->id) selected @endif>{{ $type->name }}</option>
+                        <option value="{{ $type->id }}" class="text-green-600"
+                            @if (old('type_id') == $type->id) selected @endif>{{ $type->name }}</option>
                     @endforeach
                 </select>
             </div>
-        </div>
-        <div class="py-2 px-20 sm:mx-12 text-red-600 font-semibold">
-            @error('type_id')
-                {{ $message }}
-            @enderror
-        </div>
-        <!-- Category -->
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <h2 class="text-lg font-semibold py-2">Category <span class="text-red-400">*</span></h2>
+            <div class="text-sm text-red-600 font-bold px-14">
+                @error('type_id')
+                    {{ $message }}
+                @enderror
             </div>
-            <div class="flex flex-row justify-start items-center gap-4">
-                <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                    <i class="fa-solid fa-list"></i>
-                </span>
-                <select wire:model="category_id" name="category_id" id="category_id" class="w-full pl-4 p-2 text-md rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-orange-500 focus:border-orange-500">
+            <!-- Category -->
+            <h2 class="text-lg font-bold pt-4 pb-1 px-14">Category <span class="text-red-400">*</span></h2>
+            <div class="flex gap-2">
+                <i class="bg-zinc-200 p-3 rounded-lg fa-solid fa-list"></i>
+                <select wire:model="category_id" name="category_id" id="category_id"
+                    class="w-full rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-green-500 focus:border-green-500">
                     @foreach ($categories as $category)
-                        <option value="{{ $category->id }}" class="text-orange-500"
-                                @if (old('category_id') == $category->id) selected @endif>{{ $category->name }}</option>
+                        <option value="{{ $category->id }}" class="text-green-600"
+                            @if (old('category_id') == $category->id) selected @endif>{{ $category->name }}</option>
                     @endforeach
                 </select>
             </div>
-        </div>
-        <div class="py-2 px-20 sm:mx-12 text-red-600 font-semibold">
-            @error('category_id')
-                {{ $message }}
-            @enderror
-        </div>
-        <!-- Help -->
-        @if ($show % 2 != 0)
-            <div class="text-white py-4 m-4 bg-zinc-400 rounded-lg">
-                <p class="px-4">Use Ctrl + select to select multiple tags</p>
+            <div class="text-sm text-red-600 font-bold px-14">
+                @error('category_id')
+                    {{ $message }}
+                @enderror
             </div>
-        @endif
-        <!-- Tags -->
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <h2 class="text-lg font-semibold py-2">Tags <span class="text-red-400">*</span></h2>
-            </div>
-            <div class="flex flex-row justify-start items-start gap-4">
-                <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                    <i class="fa-solid fa-tags"></i>
-                </span>
-                <select wire:model.live="selectedTags" name="selectedTags" id="selectedTags" multiple class="w-full pl-4 p-2 text-md rounded-lg bg-gray-50 border border-gray-300 text-gray-900  dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-orange-500 focus:border-orange-500">
+            <!-- Help -->
+            @if ($show % 2 != 0)
+                <div class="pt-4">
+                    <div class="bg-black text-sm text-white w-fit p-2 mx-12 rounded-lg relative">
+                        <span class="text-yellow-400 font-bold">HELP - </span> (Ctrl + Click) to select multiple tags.
+                        <button wire:click.prevent="help"><i
+                                class="fa-lg fa-solid fa-circle-xmark text-red-600 hover:text-red-400 transition duration-1000 ease-in-out absolute top-0 -right-3"
+                                title="Close"></i></button>
+                    </div>
+                </div>
+            @endif
+            <!-- Tags -->
+            <h2 class="text-lg font-bold pt-4 pb-1 px-14">Tags <span class="text-red-400">*</span></h2>
+            <div class="flex gap-2">
+                <span><i class="bg-zinc-200 p-3 rounded-lg fa-solid fa-tags"></i></span>
+                <select wire:model.live="selectedTags" name="selectedTags" id="selectedTags" multiple
+                    class="w-full rounded-lg bg-gray-50 border border-gray-300 text-gray-900  dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-green-500 focus:border-green-500">
                     @foreach ($tags as $tag)
-                        <option value="{{ $tag->id }}"
-                                @if (old('selectedTags') == $tag->id) selected @endif>{{ $tag->name }}</option>
+                        <option value="{{ $tag->id }}" @if (old('selectedTags') == $tag->id) selected @endif>
+                            {{ $tag->name }}</option>
                     @endforeach
                 </select>
             </div>
-        </div>
-        <div class="py-2 px-20 sm:mx-12 text-red-600 font-semibold">
-            @error('selectedTags')
-                {{ $message }}
-            @enderror
-        </div>
-        <!-- Url -->
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="flex flex-row px-16">
-                <h2 class="text-lg font-semibold py-2">Url
-                    @if ($inputs->count() < 5)
-                        <button type="button" wire:click="add()" class="align-middle mx-2 px-2">
-                            <i class="fa-solid fa-circle-plus" title="Add Url"></i>
-                        </button>
-                    @else
-                        <span class="text-red-400 text-xs px-4">You have reached the limit of Urls (5)</span>
-                    @endif
-                </h2>
+            <div class="text-sm text-red-600 font-bold px-14">
+                @error('selectedTags')
+                    {{ $message }}
+                @enderror
+            </div>
+            <!-- Url -->
+            <div class="flex flex-row justify-start items-center pt-4 px-14 gap-2">
+                <h2 class="text-lg font-bold">URLs</h2>
+                @if ($inputs->count() < 5)
+                    <button type="button" wire:click="add">
+                        <i class="text-green-600 hover:text-green-400 transition duration-500 ease-in-out fa-solid fa-circle-plus"
+                            title="Add Url"></i>
+                    </button>
+                @else
+                    <span class="text-sm text-red-600 font-bold">You have reached the limit of Urls (5)</span>
+                @endif
             </div>
             @php $count = 0 @endphp
             @foreach ($inputs as $key => $value)
-                <div class="flex flex-row justify-start items-center gap-4">
-                    <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                        <i class="fa-solid fa-globe"></i>
-                    </span>
-                    <input wire:model.live="inputs.{{ $key }}.url" id="inputs.{{ $key }}.url" type="text" value="{{ old('url') }}" class="w-2/3 pl-4 p-2 bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-orange-500 focus:border-orange-500">
+                <div class="flex flex-row justify-start items-center pt-2 gap-2">
+                    <i class="bg-zinc-200 p-3 rounded-lg fa-solid fa-globe"></i>
+                    <input wire:model.live="inputs.{{ $key }}.url" id="inputs.{{ $key }}.url"
+                        type="text" value="{{ old('url') }}"
+                        class="w-full rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-green-500 focus:border-green-500">
                     @if ($count > 0)
-                        <button type="button" wire:click="remove({{ $key }})" class="align-middle mx-2 px-2">
-                            <span style="font-size: 1.2rem; color: rgba(204, 13, 13, 0.849);"><i class="fa-solid fa-trash"></i></span>
+                        <button type="button" wire:click="remove({{ $key }})" title="Delete">
+                            <i
+                                class="text-red-600 hover:text-red-400 transition duration-500 ease-in-out fa-solid fa-trash"></i></span>
                         </button>
-                    @else
                     @endif
                 </div>
-                <div class="py-2 px-20 sm:mx-12 text-sm text-red-600 font-semibold">
+                <div class="text-sm text-red-600 font-bold px-14">
                     @error('inputs.' . $key . '.url')
                         {{ $message }}
                     @enderror
                 </div>
                 @php $count++ @endphp
             @endforeach
-        </div>
-        <!-- Info -->
-        {{-- <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <h2 class="text-lg font-semibold py-2">Info</h2>
-            </div>
-            <div class="flex flex-row justify-start items-start gap-4">
-                <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                    <i class="fa-solid fa-circle-info"></i>
-                </span>
-                <textarea rows="8" cols="20" wire:model="info" name="info" id="info" type="text" class="appearance-none block w-full bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-orange-500 focus:border-orange-500">{{ old('info') }}</textarea>
-            </div>
-        </div> --}}
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <h2 class="text-lg font-semibold py-2">Info</h2>
-            </div>
-            <div class="flex flex-row justify-start items-start gap-4">
-                <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                    <i class="fa-solid fa-circle-info"></i>
-                </span>
-                <div class="w-full">
+            <!-- Info -->
+            <h2 class="text-lg font-bold pt-4 pb-1 px-14">Info</h2>
+            <div class="flex gap-2">
+                <span><i class="bg-zinc-200 p-3 rounded-lg fa-solid fa-circle-info"></i></span>
+                <div class="w-full rounded-lg bg-zinc-100">
                     @livewire('quilleditor.quill')
                     {{-- <livewire:quilleditor.quill /> --}}
                 </div>
             </div>
-        </div>
-        <!-- Errors -->
-        <div class="py-2 px-20 sm:mx-12 text-red-600 font-semibold">
-            @error('info')
-                {{ $message }}
-            @enderror
-        </div>
-        <!-- Code -->
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <h2 class="text-lg font-semibold py-2">Code</h2>
+            <!-- Errors -->
+            <div class="text-sm text-red-600 font-bold px-14">
+                @error('info')
+                    {{ $message }}
+                @enderror
             </div>
-            <div class="flex flex-row justify-start items-start gap-4">
-                <span class="bg-zinc-200 px-3 py-2 rounded-lg">
-                    <i class="fa-solid fa-laptop-code"></i>
-                </span>
-                <textarea rows="8" cols="20" wire:model="code" name="code" id="code" type="text" class="appearance-none block w-full bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-orange-500 focus:border-orange-500">{{ old('code') }}</textarea>
+            <!-- Code -->
+            <h2 class="text-lg font-bold pt-4 pb-1 px-14">Code</h2>
+            <div class="flex gap-2">
+                <span><i class="bg-zinc-200 p-3 rounded-lg fa-solid fa-laptop-code"></i></span>
+                <textarea rows="8" cols="20" wire:model="code" name="code" id="code" type="text"
+                    class=" w-full rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:ring-green-500 focus:border-green-500">{{ old('code') }}</textarea>
             </div>
-        </div>
-        <!-- Errors -->
-        <div class="py-2 px-20 sm:mx-12 text-red-600 font-semibold">
-            @error('code')
-                {{ $message }}
-            @enderror
-        </div>
-        <!-- Save -->
-        <div class="py-0 px-4 sm:mx-12">
-            <div class="px-16">
-                <button type="submit" class="w-full sm:w-fit bg-black hover:bg-slate-700 text-white capitalize p-2 sm:px-4 rounded-lg shadow-none transition duration-500 ease-in-out">
-                    Save
-                    <i class="fa-solid fa-floppy-disk px-2"></i>
-                </button>
+            <!-- Errors -->
+            <div class="text-sm text-red-600 font-bold px-14">
+                @error('code')
+                    {{ $message }}
+                @enderror
             </div>
+            <!-- Save -->
+                <div class="py-6 sm:ml-12">
+                    <button type="submit"
+                        class="w-full sm:w-60 bg-black hover:bg-slate-700 text-white uppercase p-2 rounded-lg shadow-none transition duration-1000 ease-in-out">
+                        Save
+                        <i class="fa-solid fa-floppy-disk px-2"></i>
+                    </button>
+                </div>
+
+
         </div>
+
+
+
 
     </form>
 
     <!-- Footer -->
     <div class="py-4 flex flex-row justify-end items-center px-4 bg-green-600 rounded-b-lg">
         <a href="{{ route('codeentry.index') }}">
-            <i class="fa-lg fa-solid fa-backward-step text-white hover:text-black transition duration-1000 ease-in-out" title="Go Back"></i>
+            <i class="fa-lg fa-solid fa-backward-step text-white hover:text-black transition duration-1000 ease-in-out"
+                title="Go Back"></i>
         </a>
     </div>
 
